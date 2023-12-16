a3d3d193076a0f06fe9b2e4be1f05302c19f744d
EbnDaif <fadodaif@gmail.com>
2023-12-15 18:48:36 -0800

    0.8

--- API/src/controllers/userController.js
+++ API/src/controllers/userController.js
@@ -1,6 +1,5 @@
 const { User } = require("../models/userModel");
 const { paginate } = require("../utils/pagination");
-const { genUserId } = require("../utils/userIdGenerator");
 const bcrypt = require('bcrypt')
 const asyncHandler = require('express-async-handler');
 module.exports = {
@@ -13,36 +12,6 @@ module.exports = {
         const user = await User.findOne({ _id: req.params.id })
         if (!user) return res.status(404).json({ success: false, error: "user not found." })
         res.status(200).json({ success: true, data: user })
-    }),
-    createByAdminCtrl: asyncHandler(async (req, res) => {
-        const { userId, mobileNumber } = req.body
-        const existingUser = await User.findOne({ $or: [{ userId }, { mobileNumber }] })
-        if (existingUser) return res.status(400).json({ success: false, error: `This ${existingUser.userId === userId ? "userId" : "MobileNumber"} already in use ..!` })
-        let generatedUserId
-        if (userId) {
-            const user = await User.exists({ userId });
-            if (user) return res.status(400).json({ success: false, error: "userId already in use." });
-            generatedUserId = userId;
-        } else {
-            generatedUserId = await genUserId();
-        }
-        const newUser = new User({ ...req.body, userId: generatedUserId })
-        await newUser.save()
-        res.status(201).json({ success: true, data: newUser })
-    }),
-    updateByAdminCtrl: asyncHandler(async (req, res) => {
-        const { password, mobileNumber, userId } = req.body
-        let user = await User.findById(req.params.id);
-        if (!user) return res.status(404).json({ success: false, error: 'User not found' });
-        let existingUser = await User.findOne({ _id: { $ne: req.params.id }, $or: [{ userId }, { mobileNumber }] });
-        if (existingUser) return res.status(400).json({ success: false, error: `This ${existingUser.userId === userId ? "User ID" : "MobileNumber"} already in use ..!` })
-        if (password) {
-            const genSalt = await bcrypt.genSalt(10);
-            const hash = await bcrypt.hash(password, genSalt);
-            req.body.password = hash
-        }
-        const updatedUser = await User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
-        res.status(200).json({ success: true, data: updatedUser });
     }),
     updateUserProfileCtrl: asyncHandler(async (req, res) => {
         if (req.file) req.body.profileImage = `/users/${req.file.filename}`
