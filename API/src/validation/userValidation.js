83153d5c97987a7c58d7b27a615089448e636b1f
EbnDaif <fadodaif@gmail.com>
2023-12-17 09:16:29 -0800

    Update user

--- API/src/validation/userValidation.js
+++ API/src/validation/userValidation.js
@@ -2,6 +2,15 @@ const Joi = require('joi');
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
 const userIdRegex = /^[0-9]{10}$/
 module.exports = {
+  updateByAdminSchema: Joi.object({
+
+    accountStatus: Joi.string().valid('Active', 'Inactive').messages({
+        'any.only': 'Account status must be either "Active" or "Inactive".'
+    }),
+    role: Joi.string().valid('Admin', 'Editor', 'Instructor', 'Student').messages({
+        'any.only': 'Invalid role. Allowed values are: Admin, Editor, Instructor, Student.'
+    }),
+}),
   updateUserProfileSchema: Joi.object({
     nationality: Joi.string(),
     age: Joi.number().integer().messages({
