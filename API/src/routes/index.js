6e983e0c2632ec163a7f61f0bbe9fa43b5d47b46
Samir <samirkaramc97@gmail.com>
2024-06-30 19:58:35 +0300

    NewB

--- API/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const router=require("express").Router()
-router.use("/auth", require("./authRoutes"))
-router.use("/users", require("./userRoutes"))
-router.use('/article', require('./article.route'));
-router.use('/videos', require('./video.route'));
-router.use('/sounds', require('./sleep_sounds.route'));
-module.exports=router
